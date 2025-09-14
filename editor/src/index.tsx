9e839fb7dea5fdafbbad94a7f6bbe423293ece30
Logesh G <logeshg5@gmail.com>
2025-09-14 11:00:42 +0530

    Remove dead code

--- editor/src/index.tsx
+++ editor/src/index.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client"; // From React 18
 
-// import "./styles.scss";
 import "./styles.css";
 
 import { renderWidget } from "./widget";
 import { renderEditor } from "./editor";
-
-// https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
-// const rootContainer = document.getElementById("root");
-// const root = createRoot(rootContainer!)
-// root.render(React.createElement(App));
 
 (function init() {
     const editorElement = document.getElementById("editor");
